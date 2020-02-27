b727dcf633dd26bb727617e2bcaf9d8dd3afef6c
Neto <antonio.neto.03@hotmail.com>
2020-02-27 19:49:14 -0300

    Criando operação de intanciação

--- src/main/java/com/neto/cursomc/cursomc/CursomcApplication.java
+++ src/main/java/com/neto/cursomc/cursomc/CursomcApplication.java
@@ -1,13 +1,36 @@
 package com.neto.cursomc.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursomcApplication {
+import com.neto.cursomc.cursomc.domain.Categoria;
+import com.neto.cursomc.cursomc.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursomcApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaResporitory;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaResporitory.saveAll(Arrays.asList(cat1, cat2));
+		
+		
+	}
+	
+	
 
 }
