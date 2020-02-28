8e9fe9e4a4df5bffe9113be45b45e465bab4821e
Neto <antonio.neto.03@hotmail.com>
2020-02-28 13:02:34 -0300

    Estado e cidade

---
+++ src/main/java/com/neto/cursomc/cursomc/repositories/CidadeRepository.java
@@ -0,0 +1,11 @@
+package com.neto.cursomc.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.neto.cursomc.cursomc.domain.Cidade;
+
+@Repository
+public interface CidadeRepository extends JpaRepository<Cidade, Integer> {
+
+}
